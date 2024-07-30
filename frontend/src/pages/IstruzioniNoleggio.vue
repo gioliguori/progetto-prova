911ebcb1ec9abeef86d174915e42cd7b2f056cb0
Luca Pisani <lucapisani@Air-di-Luca.station>
2024-07-30 18:47:57 +0200

    il falegname

--- frontend/src/pages/IstruzioniNoleggio.vue
+++ frontend/src/pages/IstruzioniNoleggio.vue
@@ -9,34 +9,60 @@
         readonly
         class="q-mb-md"
       />
+      <q-input
+        v-model="reservationId"
+        label="ID della prenotazione"
+        readonly
+        class="q-mb-md"
+      />
       <q-btn @click="handleNoleggia" label="Noleggia" color="primary" class="q-mt-md" />
     </div>
   </q-page>
 </template>
 
 <script>
-import { defineComponent, ref, onMounted } from 'vue';
-import { useRouter } from 'vue-router'; // Importa useRouter
+import { defineComponent, ref, onMounted, watch } from 'vue';
+import { useRouter, useRoute } from 'vue-router'; // Importa useRouter e useRoute
 import imageSrc from 'src/assets/istruzioni.png'; // Importa l'immagine direttamente
 
 export default defineComponent({
   name: 'RentalInstructions',
   setup() {
     const bikeId = ref('');
+    const reservationId = ref('');
     const router = useRouter(); // Ottieni l'oggetto del router
+    const route = useRoute(); // Ottieni l'oggetto del route
 
     onMounted(() => {
-      bikeId.value = localStorage.getItem('selectedBikeId') || ''; // Recupera l'ID della bici dal localStorage
+      // Recupera l'ID della bici e l'ID della prenotazione dalla query string
+      bikeId.value = route.query.bikeId || '';
+      reservationId.value = route.query.reservationId || '';
+
+      if (reservationId.value) {
+        console.log(`Salvo l'ID della prenotazione nel local storage: ${reservationId.value}`);
+        localStorage.setItem('reservationId', reservationId.value);
+      }
     });
 
     const handleNoleggia = () => {
-      // Reindirizza alla pagina "scansionaqr" passando l'ID della bici come parametro di query
-      router.push({ name: 'QReader', query: { bikeId: bikeId.value } });
+      // Reindirizza alla pagina "QReader" passando l'ID della bici e l'ID della prenotazione come parametri di query
+      router.push({ name: 'QReader', query: { bikeId: bikeId.value, reservationId: reservationId.value } });
     };
+
+    // Watch for route changes to remove reservationId from local storage if navigating away from this component
+    watch(() => route.name, (newRouteName) => {
+      if (newRouteName !== 'QReader') {
+        console.log('Navigazione verso una pagina diversa da QReader, rimuovo l\'ID della prenotazione dal local storage');
+        localStorage.removeItem('reservationId');
+      } else {
+        console.log('Navigazione verso QReader, mantengo l\'ID della prenotazione nel local storage');
+      }
+    });
 
     return {
       imageSrc,
       bikeId,
+      reservationId,
       handleNoleggia
     };
   }
