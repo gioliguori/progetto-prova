0e1938c58c73b016134cf33c522dd585f1a0ee07
Luca Pisani <lucapisani@Air-di-Luca.station>
2024-08-02 00:49:11 +0200

    aggiunto componente

---
+++ frontend/src/components/DarkModeToggle.vue
@@ -0,0 +1,30 @@
+<template>
+    <q-btn
+      :icon="isDarkMode ? 'brightness_7' : 'brightness_4'"
+      @click="toggleDarkMode"
+      flat
+      round
+    />
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        isDarkMode: JSON.parse(localStorage.getItem('darkMode')) || this.$q.dark.isActive
+      };
+    },
+    methods: {
+      toggleDarkMode() {
+        this.$q.dark.toggle();
+        this.isDarkMode = this.$q.dark.isActive;
+        localStorage.setItem('darkMode', JSON.stringify(this.isDarkMode));
+      }
+    }
+  }
+  </script>
+  
+  <style scoped>
+  
+  </style>
+  
