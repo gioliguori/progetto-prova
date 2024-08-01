cd71f491224754b176fb645facac9fc7e6ba771d
Luca Pisani <lucapisani@Air-di-Luca.station>
2024-08-02 00:24:01 +0200

    fix

--- frontend/src/pages/ProfiloUtente.vue
+++ frontend/src/pages/ProfiloUtente.vue
@@ -1,62 +1,34 @@
 <template>
-  <q-page class="flex flex-center">
-    <q-card
-      class="q-mb-md"
-      v-bind:style="$q.screen.lt.sm ? { width: '90%' } : { width: '40%' }"
-    >
+  <q-page class="flex flex-center" :class="{'q-dark': $q.screen.dark, 'bg-light': !$q.screen.dark}">
+    <q-card class="full-size q-mb-md">
       <q-card-section class="text-center">
         <q-avatar size="100px" class="q-mb-md">
-          <img
-            src="src/assets/action.jpg"
-            style="object-fit: cover; width: 100%; height: 100%"
-          />
+          <img src="src/assets/action.jpg" style="object-fit: cover; width: 100%; height: 100%;" />
         </q-avatar>
-        <div class="text-h6">Welcome, {{ username }}</div>
+        <div :class="{'text-h6': true, 'text-primary': !$q.screen.dark, 'text-light': $q.screen.dark}">
+          Welcome, {{ username }}
+        </div>
       </q-card-section>
       <q-card-section>
-        <q-btn
-          @click="goToEditProfile"
-          label="MODIFICA PROFILO"
-          color="primary"
-          class="full-width q-mb-sm"
-        />
-
-        <q-btn
-          @click="goToHelp"
-          label="ASSISTENZA"
-          color="primary"
-          class="full-width q-mb-sm"
-        >
+        <q-btn @click="goToEditProfile" label="MODIFICA PROFILO" color="primary" class="full-width q-mb-sm" />
+        
+        <q-btn @click="goToHelp" label="ASSISTENZA" color="primary" class="full-width q-mb-sm">
           <template v-slot:append>
             <q-icon>
               <img src="src/assets/Webex.png" alt="Assistenza" class="icon" />
             </q-icon>
           </template>
         </q-btn>
-
-        <q-btn
-          @click="contactUs"
-          label="PARLA CON UN OPERATORE"
-          color="primary"
-          class="full-width q-mb-sm"
-        >
+        
+        <q-btn @click="contactUs" label="PARLA CON UN OPERATORE" color="primary" class="full-width q-mb-sm">
           <template v-slot:append>
             <q-icon>
-              <img
-                src="src/assets/operator-icon.png"
-                alt="Operatore"
-                class="icon"
-              />
+              <img src="src/assets/operator-icon.png" alt="Operatore" class="icon" />
             </q-icon>
           </template>
         </q-btn>
-
-        <q-btn
-          @click="logout"
-          label="LOGOUT"
-          color="negative"
-          class="full-width"
-        />
+        
+        <q-btn @click="logout" label="LOGOUT" color="negative" class="full-width" />
       </q-card-section>
       <q-card-section class="text-center q-mt-md">
         <div class="powered-by">
@@ -69,28 +41,27 @@
 </template>
 
 <script>
-import { defineComponent } from "vue";
-import { useRouter } from "vue-router";
+import { defineComponent } from 'vue';
+import { useRouter } from 'vue-router';
 import apiUrl from "src/api-config"; // Importa apiUrl
-
 export default defineComponent({
-  name: "UserProfile",
+  name: 'UserProfile',
   setup() {
     const router = useRouter();
-    const username = localStorage.getItem("username") || "User";
+    const username = localStorage.getItem('username') || 'User';
 
     const goToEditProfile = () => {
-      router.push({ name: "ModificaProfilo" });
+      router.push({ name: 'ModificaProfilo' });
     };
 
     const goToHelp = () => {
       // URL per aprire la chat con il bot su Webex
-      const webexChatUrl = "webex://chat?to=schianogo@webex.bot";
-      const fallbackUrl = "https://webex.com";
+      const webexChatUrl = 'webex://chat?to=schianogo@webex.bot';
+      const fallbackUrl = 'https://webex.com';
 
       // Crea un iframe invisibile per tentare di aprire l'app Webex
-      const iframe = document.createElement("iframe");
-      iframe.style.display = "none";
+      const iframe = document.createElement('iframe');
+      iframe.style.display = 'none';
       iframe.src = webexChatUrl;
       document.body.appendChild(iframe);
 
@@ -98,26 +69,25 @@ export default defineComponent({
       setTimeout(() => {
         // Verifica se la finestra ha il focus. Se no, mostra un messaggio di avviso
         if (document.hasFocus()) {
-          alert("PER CHATTARE CON IL BOT È NECESSARIO INSTALLARE L'APP WEBEX");
-          window.open(fallbackUrl, "_blank"); // Reindirizza all'URL di fallback
+          alert('PER CHATTARE CON IL BOT È NECESSARIO INSTALLARE L\'APP WEBEX');
+          window.open(fallbackUrl, '_blank'); // Reindirizza all'URL di fallback
         }
       }, 1000); // Attendi un momento per il tentativo di apertura dell'app
     };
 
     const contactUs = () => {
-      const webexCallUrl =
-        "webex://meet?url=https://meet1376.webex.com/meet/pr26349809433";
+      const webexCallUrl = 'webex://meet?url=https://meet1376.webex.com/meet/pr26349809433';
 
       window.location.href = webexCallUrl;
 
       setTimeout(() => {
-        window.open("https://meet1376.webex.com/meet/pr26349809433", "_blank");
+        window.open('https://meet1376.webex.com/meet/pr26349809433', '_blank');
       }, 500);
     };
 
     const logout = () => {
-      localStorage.removeItem("username");
-      router.push({ name: "LoginUtente" });
+      localStorage.removeItem('username');
+      router.push({ name: 'LoginUtente' });
     };
 
     return {
@@ -132,8 +102,26 @@ export default defineComponent({
 </script>
 
 <style scoped>
+.q-page {
+  height: 100vh; /* Altezza intera della finestra */
+  width: 100vw; /* Larghezza intera della finestra */
+  background-color: white; /* Sfondo bianco per la modalità chiara */
+  display: flex; /* Utilizzare Flexbox per centrare il contenuto */
+  justify-content: center; /* Centrare orizzontalmente */
+  align-items: center; /* Centrare verticalmente */
+}
+
+.q-page.q-dark {
+  background-color: #212121; /* Sfondo scuro per la modalità scura */
+}
+
 .q-card {
-  max-width: 400px;
+  width: 100%; /* Larghezza al 100% del contenitore */
+  height: 100%; /* Altezza al 100% del contenitore */
+  max-width: 600px; /* Limite massimo della larghezza */
+  max-height: 800px; /* Limite massimo dell'altezza */
+  display: flex;
+  flex-direction: column;
 }
 
 .q-card-section {
@@ -142,6 +130,10 @@ export default defineComponent({
 
 .full-width {
   width: 100%;
+}
+
+.full-height {
+  height: 100%;
 }
 
 .q-mb-sm {
@@ -176,5 +168,17 @@ export default defineComponent({
 .icon {
   width: 24px;
   height: 24px;
+}
+
+.text-primary {
+  color: #1b89ff; /* Colore del testo per la modalità chiara */
+}
+
+.text-light {
+  color: #ffffff; /* Colore del testo per la modalità scura, se necessario */
+}
+
+.bg-light {
+  background-color: white; /* Sfondo bianco per la modalità chiara */
 }
 </style>
